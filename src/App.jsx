e1ed5e1bfc40e474f8a59fe381a20912f5b8544a
Ruben Alvarez <info@rubenalvarez.dev>
2024-12-28 15:37:45 +0100

    STAGE: first commit, vite project blank

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,11 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext/AuthContext'
-import { AppProvider } from './contexts/AppContext/AppContext'
-import SessionManager from './components/session/SessionManager/SessionManager'
-import { useAuth } from './contexts/AuthContext/AuthContext'
-import Navbar from './components/layout/Navbar/Navbar'
-import Aside from './components/layout/Aside/Aside'
-import Display from './components/layout/Display/Display'
-import Home from './components/Home/Home'
-import Footer from './components/layout/Footer/Footer'
 import './App.css'
-
-const AppContent = () => {
-  const { isAuthenticated } = useAuth()
-
-  return (
-    <div className="app">
-      <Navbar />
-      <main className="main">
-        {isAuthenticated ? (
-          <>
-            <Aside />
-            <Display />
-          </>
-        ) : (
-          <Home />
-        )}
-      </main>
-      <Footer />
-    </div>
-  )
-}
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <AppProvider>
-          <SessionManager>
-            <AppContent />
-          </SessionManager>
-        </AppProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <div className="app">
+      APP
+    </div>
   )
 }
 
--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = 'http://127.0.0.1:8000/api'
-export const AUTH_ENDPOINTS = {
-    LOGIN: '/login',
-    LOGOUT: '/logout',
-    REGISTER: '/register',
-    REFRESH: '/refresh-token'
-}
